4da8557cf54684a9c2f37c44456353fdd4f9a0c5
Samuele Mattiuzzo <samumatt@gmail.com>
2022-08-14 07:22:07 +0100

    Favicon2

--- houseplants/urls.py
+++ houseplants/urls.py
@@ -1,9 +1,15 @@
 
 
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reminders.urls')),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
