bb779ec09c9101db12d6ede52198408bc9f2c2ed
Samuele Mattiuzzo <samumatt@gmail.com>
2022-08-14 18:33:14 +0100

    On demand admin

--- houseplants/urls.py
+++ houseplants/urls.py
@@ -1,12 +1,17 @@
 
 
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
 from django.views.generic.base import RedirectView
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns = []
+
+if settings.ADMIN_ENABLED is True:
+    urlpatterns += [path('admin/', admin.site.urls), ]
+
+urlpatterns += [
     path('', include('reminders.urls')),
     path(
         "favicon.ico",
